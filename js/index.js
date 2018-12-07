bb297a20a4da98e4a584fbb93d1ab6108acf2638
dada2507073071 <1430244950@qq.com>
2018-12-07 10:18:00 +0800

    wbf 12-7 js提交
    首页完整的js

---
+++ js/index.js
@@ -0,0 +1,50 @@
+var ss = document.getElementById('ss');
+var btn = document.getElementById('btn');
+var l1 = document.getElementById('l1');
+var span = document.getElementById('span2');
+var sz = document.getElementById('sz');
+var img1 = document.getElementById('img_one');
+var img2 = document.getElementById('img2');
+var img3 = document.getElementById('img3');
+var js1 = document.getElementById('js1');
+var calendar = document.getElementById('calendar');
+var d1 = document.getElementById('dd_one');
+var d2 = document.getElementById('dd_two');
+// var demo3 = document.getElementById('demo3');
+ss.onclick = function () {
+    btn.style.display = "block";
+    l1.style.display = "none";
+}
+span.onclick = function () {
+    btn.style.display = "none";
+    l1.style.display = "block";
+}
+img1.onclick = function () {
+    if (sz.style.display == 'none') {
+        sz.style.display = "block";
+    } else if (sz.style.display == 'block') {
+        sz.style.display = "none";
+    }
+}
+img2.onclick = function () {
+    if (js1.style.display == 'none') {
+        js1.style.display = "block";
+    } else if (js1.style.display == 'block') {
+        js1.style.display = "none";
+    }
+}
+img3.onclick = function () {
+    if (calendar.style.display == 'none') {
+        calendar.style.display = "block";
+    } else if (calendar.style.display == 'block') {
+        calendar.style.display = "none";
+    }
+}
+d1.onclick = function () {
+    if (d2.style.display == 'none') {
+        d2.style.display = "block";
+    } else if (d2.style.display == 'block') {
+        d2.style.display = "none";
+    }
+}
+// demo3.datepicker({});
